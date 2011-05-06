82417169794e564a3b499c632f3a3ff12b306618
unknown <Louis@.(none)>
2011-05-06 20:11:20 +0200

    vv

--- RayTracer/Draw.cs
+++ RayTracer/Draw.cs
@@ -8,7 +8,7 @@ namespace RayTracer
 {
     class Draw
     {
-        //Louis est un con
+        //Arthur est con
         public static Bitmap newImage(int width, int height, List<Sphere> l_sphere)
         {
             Bitmap image = new Bitmap(width, height);
